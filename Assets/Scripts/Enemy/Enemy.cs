9a6f5dbdd6daba5647676ce4007bb9d7b18d8c5a
TanHoang01 <tanhm2k1@gmail.com>
2024-07-03 14:31:15 +0700

    fix tower aim target

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,21 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetLock : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    [SerializeField] Transform weapon;
-    [SerializeField] Transform target;
-
     // Start is called before the first frame update
     void Start()
     {
-        target = FindAnyObjectByType<EnemyMover>().transform;
-        Debug.Log(target);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        weapon.LookAt(target);
+        
     }
 }
---
+++ Assets/Scripts/Tower/TargetLock.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+
+public class TargetLock : MonoBehaviour
+{
+    [SerializeField] Transform weapon;
+    [SerializeField] Transform target;
+    [SerializeField] float range = 15f;
+    [SerializeField] ParticleSystem towerArrowParticle;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        FindClosestEnemy();
+        FocusOnEnemy();
+    }
+
+    private void FocusOnEnemy()
+    {
+        float enemyDistance = Vector3.Distance(transform.position, target.position);
+        weapon.LookAt(target);
+        if (enemyDistance > range)
+            Attack(false);
+        else
+            Attack(true);
+    }
+
+    private void FindClosestEnemy()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null; 
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies) 
+        {
+            float enemyDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if(enemyDistance < maxDistance)
+            {
+                maxDistance = enemyDistance;
+                closestTarget = enemy.transform;
+            }
+        }
+        target = closestTarget;
+    }
+
+    private void Attack(bool isActive)
+    {
+        towerArrowParticle = GetComponentInChildren<ParticleSystem>();
+        var emissionState = towerArrowParticle.emission;
+        emissionState.enabled = isActive;
+    }
+}
