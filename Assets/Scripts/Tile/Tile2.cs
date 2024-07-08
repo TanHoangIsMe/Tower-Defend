3d909bafd36d9d16da69219c1d639f19bcc10561
TanHoang01 <tanhm2k1@gmail.com>
2024-07-08 20:21:40 +0700

    fix debug tool

--- Assets/Scripts/Tile/Tile2.cs
+++ Assets/Scripts/Tile/Tile2.cs
@@ -4,45 +4,45 @@ using UnityEngine;
 
 public class Tile2 : MonoBehaviour
 {
-     [SerializeField] Tower towerPrefab;
-[SerializeField] bool isPlaceable;
-public bool IsPlaceable { get { return isPlaceable; } }
+    [SerializeField] Tower towerPrefab;
+    [SerializeField] bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; } }
 
-GridManager gridManager;
-PathFinder pathFinder;
-Vector2Int coordinate;
+    GridManager gridManager;
+    PathFinder pathFinder;
+    Vector2Int coordinate;
 
-private void Awake()
-{
-    gridManager = FindObjectOfType<GridManager>();
-    pathFinder = FindObjectOfType<PathFinder>();
-}
-
-private void Start()
-{
-    if (gridManager != null)
+    private void Awake()
     {
-        coordinate = gridManager.GetCoordinatesFromPosition(transform.position);
-        if (!isPlaceable)
-        {
-            gridManager.BlockNode(coordinate);
-        }
+        gridManager = FindObjectOfType<GridManager>();
+        pathFinder = FindObjectOfType<PathFinder>();
     }
-}
 
-private void OnMouseDown()
-{
-    if (gridManager.GetNode(coordinate).isWalkable && !pathFinder.WillBlockPath(coordinate))
-    {
-        bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
-        isPlaceable = !isPlaced;
-        gridManager.BlockNode(coordinate);
-        Debug.Log("place");
-    }
-    else
-    {
-        Debug.Log(gridManager.GetNode(coordinate).isWalkable);
-        Debug.Log(pathFinder.WillBlockPath(coordinate));
-    }
-}
+    //private void Start()
+    //{
+    //    if (gridManager != null)
+    //    {
+    //        coordinate = gridManager.GetCoordinatesFromPosition(transform.position);
+    //        if (!isPlaceable)
+    //        {
+    //            gridManager.BlockNode(coordinate);
+    //        }
+    //    }
+    //}
+
+    //private void OnMouseDown()
+    //{
+    //    if (gridManager.GetNode(coordinate).isWalkable && !pathFinder.WillBlockPath(coordinate))
+    //    {
+    //        bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+    //        isPlaceable = !isPlaced;
+    //        gridManager.BlockNode(coordinate);
+    //        Debug.Log("place");
+    //    }
+    //    else
+    //    {
+    //        Debug.Log(gridManager.GetNode(coordinate).isWalkable);
+    //        Debug.Log(pathFinder.WillBlockPath(coordinate));
+    //    }
+    //}
 }
