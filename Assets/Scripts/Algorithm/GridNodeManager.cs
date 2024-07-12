0e333cde62c612ec7a8b242adf22bdf97b9ac1be
TanHoang01 <tanhm2k1@gmail.com>
2024-07-12 21:02:03 +0700

    fix map

--- Assets/Scripts/Algorithm/GridNodeManager.cs
+++ Assets/Scripts/Algorithm/GridNodeManager.cs
@@ -18,7 +18,7 @@ public class GridNodeManager : MonoBehaviour
 
     void CreateLeftGrid()
     {
-        for (int x = -1; x <= 5; x++)
+        for (int x = -1; x <= 4; x++)
         { 
             for(int y = 1; y <= 7; y++)
             {
